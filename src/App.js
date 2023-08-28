eaef8f730e53cb7884530f6d55aba0e02006991b
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-08-28 16:20:20 +0530

    organize-files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 import React from "react";
 
 function App() {
