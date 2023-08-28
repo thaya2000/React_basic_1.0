8ede4a4f37fff18363429c038accb73be2365787
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-08-28 16:00:45 +0530

    code-jsx-style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Expenses from "./components/Expenses";
+import React from "react";
 
 function App() {
   const expenses = [
@@ -24,12 +25,24 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
       <Expenses items={expenses} />
     </div>
   );
+
+  /*   
+  
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let's get started!"),
+    React.createElement(Expenses, { items: expenses })
+  ); 
+  
+  */
 }
 
 export default App;
