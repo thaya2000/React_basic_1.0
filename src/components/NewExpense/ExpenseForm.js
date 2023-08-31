444b73cd0c0d149ec5ba4f9b41814676d2e0cea1
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-08-31 19:30:26 +0530

    Using-one-state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,11 +2,39 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
     // console.log(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
+  };
+  const amountChangeHandler = (event) => {
+    // setEnteredAmount(event.target.value);
+    // console.log(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
+  };
+  const dateChangeHandler = (event) => {
+    // setEnteredDate(event.target.value);
+    // console.log(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
 
   return (
@@ -18,11 +46,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            onChange={dateChangeHandler}
+            min="2019-01-01"
+            max="2023-12-31"
+          />
         </div>
       </div>
       <div className="new-expense__actions">
