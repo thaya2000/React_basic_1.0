87b3c7d4244c55fcb8d3ff60676a994514ea0493
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-09-03 21:21:23 +0530

    chart_dynamic_style

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoints) => (
+        <ChartBar
+          key={dataPoints.label}
+          value={dataPoints.value}
+          maxValue={null}
+          label={dataPoints.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
